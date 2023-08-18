b89c36bf606e163ddcffcdf3f9a8f80718fbc5a9
almoralita <angielizethmora@gmail.com>
2023-08-17 23:11:09 -0500

    Update urlGit.groovy

--- vars/urlGit.groovy
+++ vars/urlGit.groovy
@@ -1,1 +1,7 @@
-mail bcc: '', body: 'Prueba', cc: '', from: '', replyTo: '', subject: 'Prueba', to: 'angielizethmora@gmail.com'
+//mail bcc: '', body: 'Prueba', cc: '', from: '', replyTo: '', subject: 'Prueba', to: 'angielizethmora@gmail.com'
+int useSomeLib(Helper helper) {
+    helper.prepare()
+    return helper.count()
+}
+
+echo useSomeLib(new Helper('some text'))
