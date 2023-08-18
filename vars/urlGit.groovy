b2c6552c1e7108f5f49c862c281a27d2e43785dc
almoralita <angielizethmora@gmail.com>
2023-08-17 23:22:54 -0500

    Update urlGit.groovy

--- vars/urlGit.groovy
+++ vars/urlGit.groovy
@@ -1,5 +1,6 @@
 def call() {
     //git url: "https://github.com/almoralita/Shared_Library.git'
-    log.info
+    Logger logger = Logger.getLogger("")
+    logger.info ("I am a test info log")
     //mail bcc: '', body: 'TEst', cc: '', from: '', replyTo: '', subject: 'Test', to: 'angielizethmora@gmail.com'
 }
